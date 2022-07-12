a41a28ab2a00453ad14f2b299af83fe5d15925bd
DnissNS <denis33-2010@mail.ru>
2022-07-13 02:25:07 +0300

    [add] BadInputException.php

--- src/Exceptions/BadInputException.php
+++ src/Exceptions/BadInputException.php
@@ -17,15 +17,15 @@ class BadInputException extends \Exception
         $this->message = 'Error: ';
         if (count($this->arguments) > 0)
         {
-            $this->message .= 'these '.implode(',', $this->arguments).' are not processed by this command; ';
+            $this->message .= 'these '.implode(',', $this->arguments)." are not processed by this command; \r\n";
         }
         if (count($this->options) > 0)
         {
-            $this->message .= 'these '.implode(',', $this->options).' are not processed by this command';
+            $this->message .= 'these '.implode(',', $this->options)." are not processed by this command; \r\n";
         }
         if (count($this->options_value) > 0)
         {
-            $this->message .= 'these '.implode(',', $this->options_value).'have values that are not processed';
+            $this->message .= 'these '.implode(',', $this->options_value)." have values that are not processed; \r\n";
         }
         return $this->message;
     }
