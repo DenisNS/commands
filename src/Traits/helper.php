621b76d81445cd336571a436a5289d41abe5f925
DnissNS <denis33-2010@mail.ru>
2022-07-13 07:37:21 +0300

    [add] trait

---
+++ src/Traits/helper.php
@@ -0,0 +1,21 @@
+<?php
+trait helper
+{
+    public static function showHelpMessage(string $commands_dir, string $commands_namespace)
+    {
+        echo "This application have follow commands: \r\n";
+        foreach (glob($commands_dir . '/*.php') as $file)
+        {
+            require_once $file;
+
+            // get the file name of the current file without the extension
+            // which is essentially the class name
+            $class = $commands_namespace . basename($file, '.php');
+            if (class_exists($class))
+            {
+                $obj = new $class;
+                $obj->help();
+            }
+        }
+    }
+}
