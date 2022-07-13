182af09d7c3c02eb2a6e2fd42e59ee6aa59b091c
DnissNS <denis33-2010@mail.ru>
2022-07-13 07:30:47 +0300

    [add] showHelpMessagee in Listen

--- src/Listen.php
+++ src/Listen.php
@@ -9,7 +9,7 @@ class Listen
     private string $signature = '';
 
 
-    public function __construct(array $args)
+    public function setArgs(array $args)
     {
         $this->args = $args;
         if (count($args) > 1)
@@ -49,5 +49,23 @@ class Listen
     public function getSignature()
     {
         return $this->signature;
+    }
+
+    public static function showHelpMessage(string $commands_dir, string $commands_namespace)
+    {
+        echo "This application have follow commands: \r\n";
+        foreach (glob($commands_dir . '/*.php') as $file)
+        {
+            require_once $file;
+
+            // get the file name of the current file without the extension
+            // which is essentially the class name
+            $class = $commands_namespace . basename($file, '.php');
+            if (class_exists($class))
+            {
+                $obj = new $class;
+                $obj->help();
+            }
+        }
     }
 }
